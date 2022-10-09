a8d6b47d9fbd0e8bd3c3e0d0b73dcb48fb95fdc1
Hamid Hassani <hamidhassani119@gmail.com>
2022-10-09 14:25:55 +0330

    tostify bcryptjs packeges added to the project

---
+++ server/middleware/authentication.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcryptjs");
+
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return reject(err);
+
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) return reject(err);
+        resolve(hash);
+      });
+    });
+  });
+};
+
+module.exports = {
+  hashPassword,
+};
